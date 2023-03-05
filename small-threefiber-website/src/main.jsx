78961e3e8b5b2fb519ce30f2dd503294a77f8ee2
raph <raphael.mottelet@protonmail.com>
2023-03-05 22:19:21 +0100

    first scene

--- small-threefiber-website/src/main.jsx
+++ small-threefiber-website/src/main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import * as THREE from 'react-three-fiber'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+function main() {
+
+  const scene = new THREE.scene();
+
+  const geometry = new THREE.SphereGeometry(3,80,80)
+  const material = new THREE.MeshStandardMaterial({
+    color: "#00ff83",
+  })
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh)
+
+  //caméra
+  const camera = THREE.PerspectiveCamera(45, 800, 600)
+  scene.add(camera)
+
+
+  //render la view
+
+  const canvas = document.querySelector('.webgl');
+  const renderer = new THREE.WebGLRenderer({canvas})
+  renderer.setSize(800,600)
+  renderer.render(scene, camera)
+
+
+
+
+  return (
+    <div>main</div>
+  )
+}
+
+export default main
